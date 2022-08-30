3c8b81cd0123775b75766246b56870e9a71f27ab
hoangphuong.nam <hoangphuong.nam@dish.com>
2022-08-30 19:24:26 +0700

    update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { filterImageFromURL, deleteLocalFiles } from "./src/util/util";
 
 (async () => {
   // Init the Express application
